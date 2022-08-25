2bb609bfc0adb11576513dc226ea98fb7dafc3ec
vkmouse <knight31241@gmail.com>
2022-08-25 15:54:42 +0800

    add console log for checking stat

--- src/Presentation/Views/SimulateResultView/SimulateResultViewModel.tsx
+++ src/Presentation/Views/SimulateResultView/SimulateResultViewModel.tsx
@@ -28,64 +28,7 @@ interface IProps {
 class SimulateReultViewModel extends Component<IProps> {
   static defaultProps = {} as IProps;
 
-  getEquipmentCost(numSuccess: number) {
-    const { numSamples } = this.props.simulateResultStore;
-    const { price }  = this.props.equipmentStore;
-    return Math.round(price * (numSamples / numSuccess));
-  }
-
-  getCosts() {
-    const { results } = this.props.simulateResultStore;
-    const materialStore = this.props.materialStore;
-    const numBlessings = this.getNumBlessings();
-
-    const costs = Array(results.length);
-    const materialCost = Array(results.length);
-    for (let i = 0; i < results.length; i++) {
-      const { refineTimes, material, numSuccess } = results[i];
-      materialCost[i] = 0;
-      if (refineTimes !== 0) {
-        if (numSuccess === 0) {
-          costs[i] = Infinity;
-          continue;
-        }
-        const materialPrice = materialStore.getPrice(material);
-        const blessingPrice = materialStore.blessing;
-        materialCost[i] += materialPrice * (refineTimes / numSuccess);
-        materialCost[i] += blessingPrice * (numBlessings[i] / numSuccess);
-        if (i > 0) {
-          materialCost[i] += materialCost[i - 1] * results[i - 1].numSuccess / numSuccess;
-        }
-        materialCost[i] = Math.round(materialCost[i]);
-        costs[i] = materialCost[i] + this.getEquipmentCost(numSuccess);
-      }
-    }
-    return costs;
-  }
-
-  getNumBlessings = () => {
-    const blessingDemand = [0, 0, 0, 0,  0,
-                            0, 0, 1, 2,  3,
-                            4, 4, 9, 15, 0,
-                            0, 0, 0, 0,  0];
-
-    const { category, level } = this.props.equipmentStore;
-    const { enableBlessings } = this.props.refineProcessStore;
-    const blessings = enableBlessings[category][level];
-    const results = this.props.simulateResultStore.results;
-    const numBlessings = Array(results.length);
-    for (let i = 0; i < results.length; i++) {
-      const { refineLevel, refineTimes } = results[i];
-      if (blessings[refineLevel]) {
-        numBlessings[i] = blessingDemand[refineLevel] * refineTimes;
-      } else {
-        numBlessings[i] = 0;
-      }
-    }
-    return numBlessings;
-  };
-
-  getTest() {
+  getResults() {
     const { results } = this.props.simulateResultStore;
     const materialStore = this.props.materialStore;
     const { category, level } = this.props.equipmentStore;
@@ -98,13 +41,20 @@ class SimulateReultViewModel extends Component<IProps> {
       enableBlessings, 
       numSamples,
       equipmentPrice});
+    console.log({
+      results, 
+      materialStore, 
+      enableBlessings, 
+      numSamples,
+      equipmentPrice,
+      calculatorCalculate: calculator.calculate()})
     return calculator.calculate();
   }
 
   render() {
     return (
       <SimulateReultView
-        results={this.getTest()}
+        results={this.getResults()}
       />
     );
   }
